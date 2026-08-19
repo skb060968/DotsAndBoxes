c996158bc17763f694ec97b52f1ba407c850fcd7
skb060968 <skb060968@gmail.com>
2026-08-20 00:10:34 +0530

    board ui

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dots-and-boxes-v23';
+const CACHE_NAME = 'dots-and-boxes-v24';
 const SHELL = [
   '/', '/index.html', '/manifest.json',
   '/icons/icon-192.png', '/icons/icon-512.png',
