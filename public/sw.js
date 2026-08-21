d5ea9ad65bc74f3f3b712766c6d58173c4793a81
skb060968 <skb060968@gmail.com>
2026-08-21 09:21:02 +0530

    final V34 release

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dots-and-boxes-v33';
+const CACHE_NAME = 'dots-and-boxes-v34';
 const SHELL = [
   '/', '/index.html', '/manifest.json',
   '/icons/icon-192.png', '/icons/icon-512.png',
