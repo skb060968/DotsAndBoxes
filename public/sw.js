6ecb33032a97dcf76673d8e96f872a3c62b07520
skb060968 <skb060968@gmail.com>
2026-08-22 13:31:42 +0530

    Livekit Voicechat V40

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dots-and-boxes-v39';
+const CACHE_NAME = 'dots-and-boxes-v40';
 const SHELL = [
   '/', '/index.html', '/manifest.json',
   '/icons/icon-192.png', '/icons/icon-512.png',
